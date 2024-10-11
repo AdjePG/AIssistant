fb89660113398b49ced50306f8921b1fe1115538
Adri PG <adriprog2002@gmail.com>
2024-10-11 14:13:15 +0200

    Remove ads

--- src/components/AdBanner/AdBanner.tsx
+++ src/components/AdBanner/AdBanner.tsx
@@ -1,13 +1,15 @@
 export default function AdBanner() {
     return (
-        <div className="h-28 w-[1100px] py-2 mx-auto">
-            <ins 
-                className="adsbygoogle block"
-                data-ad-client="ca-pub-2419707164621577"
-                data-ad-slot="5599751628"
-                data-ad-format="auto"
-                data-full-width-responsive="true">
-            </ins>
-        </div>
+        <>
+        </>
+        // <div className="h-28 w-[1100px] py-2 mx-auto">
+        //     <ins 
+        //         className="adsbygoogle block"
+        //         data-ad-client="ca-pub-2419707164621577"
+        //         data-ad-slot="5599751628"
+        //         data-ad-format="auto"
+        //         data-full-width-responsive="true">
+        //     </ins>
+        // </div>
     )
 }
